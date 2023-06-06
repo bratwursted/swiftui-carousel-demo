af2a00480be7b1b042badfe86513ff2601f4273b
bratwursted <joe.bramhall@nytimes.com>
2023-06-06 06:59:59 -0400

    new xcode project

---
+++ swiftui-carousel-demo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  swiftui-carousel-demo
+//
+//  Created by Bramhall, Joe on 6/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftui-carousel-demo/swiftui_carousel_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_carousel_demoApp.swift
+//  swiftui-carousel-demo
+//
+//  Created by Bramhall, Joe on 6/6/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_carousel_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
